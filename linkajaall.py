db49675ee1ee7efff5cadbdfe16b72c3e2e27d6f
ppmmmmpp1212 <analyticsmmpp@gmail.com>
2025-06-18 11:01:01 +0800

    Update linkajaall.py

--- linkajaall.py
+++ linkajaall.py
@@ -292,17 +292,23 @@ def fetch_daily_summary(start_date, end_date, selected_transaction_types_ngrs, s
                 AND CAST(Debit AS FLOAT64) != 0
             GROUP BY DATE(InitiateDate)
         ),
-        AlfredLinkAja AS (
+        WITH AlfredLinkAja AS (
             SELECT 
                 DATE(InitiateDate) AS date,
                 COUNT(*) AS alfred_count,
-                COALESCE(SUM(CAST(Credit AS FLOAT64)), 0) AS alfred_amount
+                COALESCE(SUM(SAFE_CAST(Credit AS FLOAT64)), 0) AS alfred_amount
             FROM `alfred-analytics-406004.analytics_alfred.alfred_linkaja`
-            WHERE DATE(InitiateDate) BETWEEN DATE('{start_date}') AND DATE('{end_date}')
-                AND ClusterID IN ({', '.join(map(str, selected_cluster_ids))})
-                AND TransactionScenario = 'Digipos B2B Transfer'
-                AND CAST(Credit AS FLOAT64) != 0
+            WHERE 
+                InitiateDate IS NOT NULL
+                AND DATE(InitiateDate) BETWEEN DATE(@start_date) AND DATE(@end_date)
+                AND ClusterID IN UNNEST(@selected_cluster_ids)
+                AND TransactionScenario IN ('Digipos B2B Transfer', 'Digipos B2B Transfer In Cluster')
+                AND SAFE_CAST(Credit AS FLOAT64) IS NOT NULL
+                AND SAFE_CAST(Credit AS FLOAT64) != 0
             GROUP BY DATE(InitiateDate)
+        )
+        SELECT * FROM AlfredLinkAja
+        ORDER BY date
         ),
         AlfredReversal AS (
             SELECT 
